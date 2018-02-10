266d9338c7e8d7078ec9fea1696138c3d83492cc
Clare Keen <chrisnewyt@gmail.com>
2018-02-10 19:22:01 +0000

    Updated With More Helpful Features Including Announce And ECHO

---
+++ CommunityBot/Modules/Admin.cs
@@ -0,0 +1,61 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace CommunityBot.Modules
+{
+    public class Admin : ModuleBase<SocketCommandContext>
+    {
+        [Command("kick")]
+        [Remarks("Kick A User")]
+        [RequireUserPermission(Discord.GuildPermission.KickMembers)]
+        public async Task Kick(SocketGuildUser user)
+        {
+            await user.KickAsync();
+        }
+
+        [Command("announce")]
+        [Remarks("Make A Announcement")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task Announce([Remainder]string announcement)
+        {
+            Random random = new Random();
+
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.WithTitle("Announcement By " + Context.Message.Author.ToString());
+            embed.WithDescription(announcement);
+            embed.WithColor(0, 125, 255);
+            embed.WithCurrentTimestamp();
+
+            await Context.Channel.SendMessageAsync("", false, embed);
+            await Context.Message.DeleteAsync();
+        }
+
+        [Command("echo")]
+        [Remarks("Make The Bot Say A Message")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task Echo([Remainder] string message)
+        {
+
+            Random random = new Random();
+
+
+
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.WithTitle("Message by: " + Context.Message.Author.Username);
+            embed.WithDescription(message);
+            embed.WithColor(0, 125, 255);
+            embed.WithCurrentTimestamp();
+
+            await Context.Channel.SendMessageAsync("", false, embed);
+            await Context.Message.DeleteAsync();
+        }
+    }
+}
