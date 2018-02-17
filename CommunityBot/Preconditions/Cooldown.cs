df998c25ceef90020af2bf5aa142f7d69d1ec0ab
PezeM <pezemos@gmail.com>
2018-02-17 15:52:24 +0100

    Added Cooldown precondition attribute.

---
+++ CommunityBot/Preconditions/Cooldown.cs
@@ -0,0 +1,67 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace CommunityBot.Preconditions
+{
+    public class Cooldown : PreconditionAttribute
+    {
+        TimeSpan CooldownLength { get; set; }
+        bool LimitForAdmins { get; set; }
+        readonly ConcurrentDictionary<CooldownInfo, DateTime> _cooldowns = new ConcurrentDictionary<CooldownInfo, DateTime>();
+
+        /// <summary>
+        /// Sets the cooldown for a user to use this command
+        /// </summary>
+        /// <param name="seconds">Sets the cooldown in seconds.</param>
+        /// <param name="limitForAdmins">Set whether admins should have cooldowns between commands use.</param>
+        public Cooldown(int seconds, bool limitForAdmins = false)
+        {
+            CooldownLength = TimeSpan.FromSeconds(seconds);
+            LimitForAdmins = limitForAdmins;
+        }
+
+        public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            // Check if the user if administrator and if it needs to apply cooldown for him.
+            var user = context.User is IGuildUser ? (IGuildUser)context.User : null;
+            if (!LimitForAdmins && user.GuildPermissions.Administrator && user != null)
+                return Task.FromResult(PreconditionResult.FromSuccess());
+
+            var key = new CooldownInfo(context.User.Id, command.GetHashCode());
+            // Check if message with the same hash code is already in dictionary 
+            if (_cooldowns.TryGetValue(key, out DateTime endsAt))
+            {
+                // Calculate the difference between current time and the time cooldown should end
+                var difference = endsAt.Subtract(DateTime.UtcNow);
+                // Display message if command is on cooldown
+                if (difference.Ticks > 0)
+                {
+                    return Task.FromResult(PreconditionResult.FromError($"You can use this command in {difference.ToString(@"mm\:ss")}"));
+                }
+                // Update cooldown time
+                var time = DateTime.UtcNow.Add(CooldownLength);
+                _cooldowns.TryUpdate(key, time, endsAt);
+            }
+            else
+            {
+                _cooldowns.TryAdd(key, DateTime.UtcNow.Add(CooldownLength));
+            }
+
+            return Task.FromResult(PreconditionResult.FromSuccess());
+        }
+        public struct CooldownInfo
+        {
+            public ulong UserId { get; }
+            public int CommandHashCode { get; }
+
+            public CooldownInfo(ulong userId, int commandHashCode)
+            {
+                UserId = userId;
+                CommandHashCode = commandHashCode;
+            }
+        }
+    }
+}
