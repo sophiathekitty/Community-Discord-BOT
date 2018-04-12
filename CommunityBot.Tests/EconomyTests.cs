1dc2bf39ddab456f2f5b6c8dc2e08f28aedeaf72
Petr Sedláček <petr.spelos@gmail.com>
2018-04-12 12:51:12 +0200

    Replace Unit tests with an example test && added nuget package to make Unit tests work

--- CommunityBot.Tests/EconomyTests.cs
+++ CommunityBot.Tests/EconomyTests.cs
@@ -5,11 +5,9 @@ namespace CommunityBot.Tests
     public class EconomyTests
     {
         [Test]
-        public void EconomyReactionTest([Values(0, 201, 551, 801, 1101, 2501, 5001, 10001, 20001, 50001, 100001)]ulong value)
+        public void FirstUnitTest()
         {
-            var actual = Global.GetMiuniesCountReaction(value, string.Empty);
-            Assert.NotNull(actual);
-            Assert.AreNotEqual(string.Empty, actual);;
+            Assert.Pass();
         }
     }
 }
