4c8d55f49b6763013b51ca4cffded529ba5fa2f3
= <benkosmann@gmail.com>
2018-05-09 20:41:42 +0200

    Command "Daily" update
    Now shows the remaining time till the daily refreshes if used during the cooldown time.

--- CommunityBot/Features/Economy/Daily.cs
+++ CommunityBot/Features/Economy/Daily.cs
@@ -10,19 +10,22 @@ namespace CommunityBot.Features.Economy
 {
     public static class Daily
     {
-        public enum DailyResult { Success, AlreadyRecieved }
+        public struct DailyResult {
+            public bool Success;
+            public TimeSpan RefreshTimeSpan;
+        }
 
         public static DailyResult GetDaily(ulong userId)
         {
             var account = GlobalUserAccounts.GetUserAccount(userId);
-            var difference = DateTime.Now - account.LastDaily;
+            var difference = DateTime.UtcNow - account.LastDaily.AddDays(1);
 
-            if (difference.TotalHours < 24) return DailyResult.AlreadyRecieved;
+            if (difference.TotalHours < 0) return new DailyResult { Success = false, RefreshTimeSpan = difference };
 
             account.Miunies += Constants.DailyMuiniesGain;
             account.LastDaily = DateTime.UtcNow;
             GlobalUserAccounts.SaveAccounts(userId);
-            return DailyResult.Success;
+            return new DailyResult { Success = true };
         }
     }
 }
