b64116607a64f65d05f6651542f453d7690dee9f
Petr Sedláček <petr.spelos@gmail.com>
2018-09-16 09:35:13 +0200

    Update deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 cd ~/NetCore/Community-Discord-BOT/src/CommunityBot
-dotnet publish CommunityBot -r win-arm -c Release
-ssh Administrator@192.168.0.101 "kill.exe CommunityBot"
-scp -r ~/NetCore/Community-Discord-BOT/CommunityBot/bin/Release/netcoreapp2.0/win-arm/publish/* Administrator@192.168.0.101:./../../../miunie
-ssh Administrator@192.168.0.101
+dotnet publish CommunityBot -r linux-arm -c Release
+ssh pi@192.168.0.101 "pkill CommunityBot"
+scp -r ~/NetCore/Community-Discord-BOT/CommunityBot/bin/Release/netcoreapp2.0/linux-arm/publish/* pi@192.168.0.101:~/bots/miunie
+ssh pi@192.168.0.101
