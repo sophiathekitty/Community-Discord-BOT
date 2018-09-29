219412dd0037fd229253f841b79719d38f6d6de2
Charly6596 <cgutisanc@gmail.com>
2018-09-29 17:13:31 +0200

    Format changes to make Codacy happy

--- CommunityBot/Extensions/ContextExt.cs
+++ CommunityBot/Extensions/ContextExt.cs
@@ -7,11 +7,15 @@ namespace CommunityBot.Extensions
 {
     public class ContextExt : SocketCommandContext
     {
-        public readonly GlobalUserAccount UserAccount;
+        public readonly GlobalUserAccount UserAccount {get;}
         
         public ContextExt(DiscordSocketClient client, SocketUserMessage msg) : base(client, msg)
         {
-            if (User is null) { return; }
+            if (User is null) 
+            { 
+                return; 
+            }
+            
             UserAccount = GlobalUserAccounts.GetUserAccount(User);
             
             var commandUsedInformation = new CommandInformation(msg.Content, msg.CreatedAt.DateTime);
