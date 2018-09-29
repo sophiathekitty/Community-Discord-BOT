350eb84185d4b0d6614696b7126697fa633784d6
Charly6596 <cgutisanc@gmail.com>
2018-09-29 16:14:40 +0200

    Add Command History to the GlobalUserAccount. Add GlobalUserAccount to an extension of the SocketCommandContext.

---
+++ CommunityBot/Extensions/ContextExt.cs
@@ -0,0 +1,24 @@
+﻿using CommunityBot.Entities;
+using CommunityBot.Features.GlobalAccounts;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace CommunityBot.Extensions
+{
+    public class ContextExt : SocketCommandContext
+    {
+        public readonly GlobalUserAccount UserAccount;
+        
+        public ContextExt(DiscordSocketClient client, SocketUserMessage msg) : base(client, msg)
+        {
+            if (User is null) return;
+            UserAccount = GlobalUserAccounts.GetUserAccount(User);
+            
+            var commandUsedInformation = new CommandInformation(msg.Content, msg.CreatedAt.DateTime);
+            
+            UserAccount.AddCommandToHistory(commandUsedInformation);
+
+            GlobalUserAccounts.SaveAccounts(UserAccount.Id);
+        }
+    }
+}
